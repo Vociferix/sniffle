6bdd38f5acd44781e0bfaa47c4d89f8bbcafa878
Jack Bernard <jack.a.bernard.jr@gmail.com>
2023-02-10 10:39:45 -0600

    fixed example to work with async changes

--- examples/pkt-dump.rs
+++ examples/pkt-dump.rs
@@ -1,22 +1,24 @@
 use sniffle::prelude::*;
+use sniffle::Error;
 use std::io::Write;
 
-fn dump<S: Sniff>(mut sniffer: S) -> Result<(), SniffError> {
+async fn dump<S: Sniff>(mut sniffer: S) -> Result<(), Error> {
     let stdout = std::io::stdout();
     let mut dumper = LogDumper::new(stdout.lock());
     let mut first = true;
-    for pkt in sniffer.iter() {
+    while let Some(pkt) = sniffer.sniff().await? {
         if !first {
             writeln!(dumper.as_inner_mut().as_inner_mut())?;
         } else {
             first = false;
         }
-        pkt?.dump(&mut dumper)?;
+        pkt.dump(&mut dumper)?;
     }
     Ok(())
 }
 
-fn main() -> Result<(), SniffError> {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let src = std::env::args().nth(1).unwrap_or_else(|| {
         panic!("Expected one argument of either an interface name or a capture file path");
     });
@@ -26,8 +28,8 @@ fn main() -> Result<(), SniffError> {
                 .promiscuous_mode(true)
                 .immediate_mode(true)
                 .open()?;
-            dump(sniffer)
+            dump(sniffer).await
         }
-        None => dump(FileSniffer::open(&src[..])?),
+        None => dump(FileSniffer::open(&src[..]).await?).await,
     }
 }
